ab34fd16ea7dac503fb5c23d75655ec16cbad95b
Anmol Durgapal <slothfulwave10@gmail.com>
2020-07-08 19:57:53 +0530

    Update py_code_2.py

--- Plotly/05. Box Plots/py_code_2.py
+++ Plotly/05. Box Plots/py_code_2.py
@@ -14,7 +14,7 @@ y = [1,14,14,15,16,18,18,19,19,20,20,23,24,26,27,27,28,29,33,54]
 ## creating a data list
 data = [go.Box(
     y = y,
-    boxpoints = 'all',    ## to show the outliers
+    boxpoints = 'all',    ## to show all points
     jitter = 0.4,         ## spread the data points
     pointpos = 1.5        ## offset them to the right of the box
 )]
